1dde3672760d4d24e644c96eee601aebe0ca163f
hosimiya7 <nerokarurin@gmail.com>
2021-11-02 22:47:24 +0900

    class化

--- index.js
+++ index.js
@@ -1,65 +1,97 @@
-
-
-let point = 1
-document.getElementById("point").innerHTML = point
-
 document.getElementById("start").onclick = function () {
 
-    let random_num = Math.floor(Math.random() * 13) + 1;
-    document.getElementById("number").innerHTML = random_num
+    const highAndLow = new HighAndLow
 
-    let next_num = Math.floor(Math.random() * 13) + 1;
-
+    highAndLow.startGame()
 
     document.getElementById("high").onclick = function () {
-        if (random_num <= next_num) {
-            document.getElementById("next_number").innerHTML = next_num
-            point *= 2
-            document.getElementById("point").innerHTML = point
-            random_num = next_num
-            document.getElementById("number").innerHTML = random_num
-            next_num = Math.floor(Math.random() * 13) + 1;
-            document.getElementById("next_number").innerHTML = null
+        if (highAndLow.random_num <= highAndLow.next_num) {
+            highAndLow.getPoint()
+            highAndLow.playGame()
+
         } else {
-            document.getElementById("next_number").innerHTML = next_num
-            point = 0
-            document.getElementById("point").innerHTML = point
-            random_num = next_num
-            document.getElementById("number").innerHTML = random_num
-            next_num = Math.floor(Math.random() * 13) + 1;
-            document.getElementById("next_number").innerHTML = null
+            highAndLow.lostPoint()
+            highAndLow.playGame()
         }
     }
 
     document.getElementById("low").onclick = function () {
-        if (random_num >= next_num) {
-            document.getElementById("next_number").innerHTML = next_num
-            point *= 2
-            document.getElementById("point").innerHTML = point
-            random_num = next_num
-            document.getElementById("number").innerHTML = random_num
-            next_num = Math.floor(Math.random() * 13) + 1;
-            document.getElementById("next_number").innerHTML = null
+        if (highAndLow.random_num >= highAndLow.next_num) {
+            highAndLow.getPoint()
+            highAndLow.playGame()
 
         } else {
-            document.getElementById("next_number").innerHTML = next_num
-            point = 0
-            document.getElementById("point").innerHTML = point
-            random_num = next_num
-            document.getElementById("number").innerHTML = random_num
-            next_num = Math.floor(Math.random() * 13) + 1;
-            document.getElementById("next_number").innerHTML = null
+            highAndLow.lostPoint()
+            highAndLow.playGame()
         }
+    }
+    document.getElementById("reset").onclick = function () {
+        highAndLow.resetGame()
     }
 }
 
-document.getElementById("reset").onclick = function () {
-    point = 1
-    document.getElementById("point").innerHTML = point
-    document.getElementById("number").innerHTML = null
+
+class HighAndLow {
+
+    random
+    next_num
+    point
+
+    constructor() {
+        this.random_num = this.setRandomNumber()
+        this.next_num = this.setRandomNumber()
+        this.point = 1
+    }
+
+    setRandomNumber() {
+        return Math.floor(Math.random() * 13) + 1;
+    }
+
+    startGame(){
+        this.setNumber()
+        this.setPoint()
+    }
+
+    playGame(){
+        this.getNextnumber()
+        this.setNumber()
+        this.setPoint()
+    }
+
+    resetGame(){
+        this.resetPoint()
+        this.clearNumber()
+        this.setPoint()
+    }
+
+    setNumber() {
+        document.getElementById("number").innerHTML = this.random_num
+    }
+
+    setPoint() {
+        document.getElementById("point").innerHTML = this.point
+    }
+
+    getNextnumber() {
+        this.random_num = this.next_num
+        this.next_num = this.setRandomNumber()
+    }
+
+    getPoint() {
+        this.point *= 2
+    }
+
+    lostPoint() {
+        this.point = 0
+    }
+
+    resetPoint() {
+        this.point = 1
+    }
+
+    clearNumber(){
+        document.getElementById("number").innerHTML = null
+    }
 }
 
-function setRandomNumber() {
-    return Math.floor(Math.random() * 13) + 1;
-}
 
